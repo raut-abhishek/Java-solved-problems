f16b1dc4e91abd4156eefab225ae097e6e8a0116
raut-abhishek <iamabhishekraut01@gmail.com>
2026-03-14 23:05:08 +0530

    Get factorial of a nummber

---
+++ Questions/Factorial.java
@@ -0,0 +1,13 @@
+public class Factorial {
+    static int getFactorial(int num){
+        int fact = 1;
+        for(int i = 1; i<= num; i++){
+            fact = fact * i;
+        }
+
+        return fact;
+    }
+    public static void main(String[] args) {
+        System.out.println(getFactorial(5));
+    }
+}
